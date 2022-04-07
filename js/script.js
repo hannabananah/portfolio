bf355a576e2d027044dbdf05998697fce423620f
hannabananah <hanna0175@gmail.com>
2022-04-07 23:44:13 +0900

    project1 mockup scroll and gnb effect

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@ topmenu.click(function (e) {
 	let index = target.index(); //인덱스를 부여하여 저장
 	let section = $(".section").eq(index);
 	let offset = section.offset().top; //offset() : 요소의 위치(문서)
-	$("html, body").animate({ scrollTop: offset }, 1000, "easeOutCirc"); //offset 값을 scrollTop에 대입(애니메이션)
+	$("html, body").animate({ scrollTop: offset }, 2000, "easeOutCirc"); //offset 값을 scrollTop에 대입(애니메이션)
 });
 
 // 스크롤이벤트
 $(window).scroll(function(){
     let scrollTop=$(window).scrollTop();
-    if (scrollTop >= $("#section1").offset().top - 500) {
+    if (scrollTop+500 >= $("#section1").offset().top - 500) {
         $("nav ul.gnb li").eq(0).addClass("active").siblings().removeClass("active");
     }
     if (scrollTop >= $("#section2").offset().top - 500) {
@@ -31,13 +31,19 @@ $(window).scroll(function(){
     if (scrollTop >= $("#section6").offset().top - 500) {
         $("nav ul.gnb li").eq(5).addClass("active").siblings().removeClass("active");
     }
-    if (scrollTop >= $("#section7").offset().top - 500) {
-        $("nav ul.gnb li").eq(6).addClass("active").siblings().removeClass("active");
-    }
-    if (scrollTop >= $("#section8").offset().top - 500) {
-        $("nav ul.gnb li").eq(7).addClass("active").siblings().removeClass("active");
-    }
-    if (scrollTop >= $("#section9").offset().top - 500) {
-        $("nav ul.gnb li").eq(8).addClass("active").siblings().removeClass("active");
-    }
-})();
+})
+
+// 목업스크롤이벤트
+$(".hidden").hover(
+    function(){
+        let ah=$(this).innerHeight();
+        let img=$(this).find('img');
+        let imgh=img.innerHeight();
+        // console.log(`a높이:${ah}img높이:${imgh}`);
+        img.stop().animate({top:ah-imgh},8000)
+    },
+	function () {
+        let img = $(this).find("img")
+        img.stop().animate({ top: 0 }, 5000);
+      }
+    )
